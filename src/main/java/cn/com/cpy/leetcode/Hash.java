3c37648f1f1ebad89a1bd7440fab36eaf22c6041
chenpanyu <chenpanyu0324@sina.com>
2018-12-10 09:52:57 +0800

    提交Hash.java

---
+++ src/main/java/cn/com/cpy/leetcode/Hash.java
@@ -0,0 +1,37 @@
+package cn.com.cpy.leetcode;
+
+/**
+ * HashMap的hash方法测试
+ *
+ * @author chenpanyu
+ * @version 1.0
+ * @since 18-11-2 上午10:30
+ */
+public class Hash {
+
+    public static void main(String[] args) {
+        hash("222");
+    }
+
+    private static int hash(Object k) {
+        int h = 0;
+        //得到k的hashcode值
+        System.out.println(Integer.toBinaryString(k.hashCode()));
+        h ^= k.hashCode();
+        System.out.println(Integer.toBinaryString(h));
+        System.out.println(Integer.toBinaryString(h >>> 20));
+        System.out.println(Integer.toBinaryString(h >>> 12));
+        System.out.println(Integer.toBinaryString((h >>> 20) ^ (h >>> 12)));
+        //进行计算
+        h ^= (h >>> 20) ^ (h >>> 12);
+        System.out.println(Integer.toBinaryString(h));
+        System.out.println(Integer.toBinaryString(h >>> 7));
+        System.out.println(Integer.toBinaryString(h >>> 4));
+        System.out.println(Integer.toBinaryString((h >>> 7) ^ (h >>> 4)));
+        h ^= (h >>> 7) ^ (h >>> 4);
+        System.out.println(Integer.toBinaryString(h));
+        return h;
+    }
+}
+
+
